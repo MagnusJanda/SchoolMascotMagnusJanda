1f8f817154c29f1550ad76e6be1d811ba0b89749
MagnusJanda <Magnus.janda@ocsbstudent.ca>
2018-02-15 14:26:29 -0500

    Initial commit

---
+++ SchoolMascotMagnusJanda/SchoolMascotForm.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace SchoolMascotMagnusJanda
+{
+    partial class frmSchoolMascot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSchoolMascot));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.schoolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.magicToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.celticsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuggetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bucksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblMagic = new System.Windows.Forms.Label();
+            this.raptorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lakersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clipppersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sunsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hawksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.heatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hornetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rocketsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.knicksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.netsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mavericksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thunderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cavsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.warriorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bullsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.schoolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1019, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // schoolsToolStripMenuItem
+            // 
+            this.schoolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.magicToolStripMenuItem,
+            this.celticsToolStripMenuItem,
+            this.nuggetsToolStripMenuItem,
+            this.bucksToolStripMenuItem,
+            this.raptorsToolStripMenuItem,
+            this.lakersToolStripMenuItem,
+            this.clipppersToolStripMenuItem,
+            this.kingsToolStripMenuItem,
+            this.sunsToolStripMenuItem,
+            this.hawksToolStripMenuItem,
+            this.heatToolStripMenuItem,
+            this.hornetsToolStripMenuItem,
+            this.rocketsToolStripMenuItem,
+            this.spursToolStripMenuItem,
+            this.knicksToolStripMenuItem,
+            this.netsToolStripMenuItem,
+            this.mavericksToolStripMenuItem,
+            this.thunderToolStripMenuItem,
+            this.cavsToolStripMenuItem,
+            this.ersToolStripMenuItem,
+            this.warriorsToolStripMenuItem,
+            this.bullsToolStripMenuItem});
+            this.schoolsToolStripMenuItem.Name = "schoolsToolStripMenuItem";
+            this.schoolsToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.schoolsToolStripMenuItem.Text = "Teams";
+            this.schoolsToolStripMenuItem.Click += new System.EventHandler(this.schoolsToolStripMenuItem_Click);
+            // 
+            // magicToolStripMenuItem
+            // 
+            this.magicToolStripMenuItem.Name = "magicToolStripMenuItem";
+            this.magicToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.magicToolStripMenuItem.Text = "Magic";
+            this.magicToolStripMenuItem.Click += new System.EventHandler(this.magicToolStripMenuItem_Click);
+            // 
+            // celticsToolStripMenuItem
+            // 
+            this.celticsToolStripMenuItem.Name = "celticsToolStripMenuItem";
+            this.celticsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.celticsToolStripMenuItem.Text = "Celtics";
+            this.celticsToolStripMenuItem.Click += new System.EventHandler(this.celticsToolStripMenuItem_Click);
+            // 
+            // nuggetsToolStripMenuItem
+            // 
+            this.nuggetsToolStripMenuItem.Name = "nuggetsToolStripMenuItem";
+            this.nuggetsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nuggetsToolStripMenuItem.Text = "Nuggets";
+            this.nuggetsToolStripMenuItem.Click += new System.EventHandler(this.nuggetsToolStripMenuItem_Click);
+            // 
+            // bucksToolStripMenuItem
+            // 
+            this.bucksToolStripMenuItem.Name = "bucksToolStripMenuItem";
+            this.bucksToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bucksToolStripMenuItem.Text = "Bucks";
+            this.bucksToolStripMenuItem.Click += new System.EventHandler(this.bucksToolStripMenuItem_Click);
+            // 
+            // lblMagic
+            // 
+            this.lblMagic.AutoSize = true;
+            this.lblMagic.BackColor = System.Drawing.Color.Transparent;
+            this.lblMagic.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMagic.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblMagic.Location = new System.Drawing.Point(178, 200);
+            this.lblMagic.Name = "lblMagic";
+            this.lblMagic.Size = new System.Drawing.Size(679, 108);
+            this.lblMagic.TabIndex = 1;
+            this.lblMagic.Text = "Orlando Magic";
+            // 
+            // raptorsToolStripMenuItem
+            // 
+            this.raptorsToolStripMenuItem.Name = "raptorsToolStripMenuItem";
+            this.raptorsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.raptorsToolStripMenuItem.Text = "Raptors";
+            this.raptorsToolStripMenuItem.Click += new System.EventHandler(this.raptorsToolStripMenuItem_Click);
+            // 
+            // lakersToolStripMenuItem
+            // 
+            this.lakersToolStripMenuItem.Name = "lakersToolStripMenuItem";
+            this.lakersToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.lakersToolStripMenuItem.Text = "Lakers";
+            this.lakersToolStripMenuItem.Click += new System.EventHandler(this.lakersToolStripMenuItem_Click);
+            // 
+            // clipppersToolStripMenuItem
+            // 
+            this.clipppersToolStripMenuItem.Name = "clipppersToolStripMenuItem";
+            this.clipppersToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clipppersToolStripMenuItem.Text = "Clipppers";
+            this.clipppersToolStripMenuItem.Click += new System.EventHandler(this.clipppersToolStripMenuItem_Click);
+            // 
+            // kingsToolStripMenuItem
+            // 
+            this.kingsToolStripMenuItem.Name = "kingsToolStripMenuItem";
+            this.kingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.kingsToolStripMenuItem.Text = "Kings";
+            this.kingsToolStripMenuItem.Click += new System.EventHandler(this.kingsToolStripMenuItem_Click);
+            // 
+            // sunsToolStripMenuItem
+            // 
+            this.sunsToolStripMenuItem.Name = "sunsToolStripMenuItem";
+            this.sunsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sunsToolStripMenuItem.Text = "Suns";
+            this.sunsToolStripMenuItem.Click += new System.EventHandler(this.sunsToolStripMenuItem_Click);
+            // 
+            // hawksToolStripMenuItem
+            // 
+            this.hawksToolStripMenuItem.Name = "hawksToolStripMenuItem";
+            this.hawksToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.hawksToolStripMenuItem.Text = "Hawks";
+            this.hawksToolStripMenuItem.Click += new System.EventHandler(this.hawksToolStripMenuItem_Click);
+            // 
+            // heatToolStripMenuItem
+            // 
+            this.heatToolStripMenuItem.Name = "heatToolStripMenuItem";
+            this.heatToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.heatToolStripMenuItem.Text = "Heat";
+            this.heatToolStripMenuItem.Click += new System.EventHandler(this.heatToolStripMenuItem_Click);
+            // 
+            // hornetsToolStripMenuItem
+            // 
+            this.hornetsToolStripMenuItem.Name = "hornetsToolStripMenuItem";
+            this.hornetsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.hornetsToolStripMenuItem.Text = "Hornets";
+            this.hornetsToolStripMenuItem.Click += new System.EventHandler(this.hornetsToolStripMenuItem_Click);
+            // 
+            // rocketsToolStripMenuItem
+            // 
+            this.rocketsToolStripMenuItem.Name = "rocketsToolStripMenuItem";
+            this.rocketsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.rocketsToolStripMenuItem.Text = "Rockets";
+            this.rocketsToolStripMenuItem.Click += new System.EventHandler(this.rocketsToolStripMenuItem_Click);
+            // 
+            // spursToolStripMenuItem
+            // 
+            this.spursToolStripMenuItem.Name = "spursToolStripMenuItem";
+            this.spursToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.spursToolStripMenuItem.Text = "Spurs";
+            this.spursToolStripMenuItem.Click += new System.EventHandler(this.spursToolStripMenuItem_Click);
+            // 
+            // knicksToolStripMenuItem
+            // 
+            this.knicksToolStripMenuItem.Name = "knicksToolStripMenuItem";
+            this.knicksToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.knicksToolStripMenuItem.Text = "Knicks";
+            // 
+            // netsToolStripMenuItem
+            // 
+            this.netsToolStripMenuItem.Name = "netsToolStripMenuItem";
+            this.netsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.netsToolStripMenuItem.Text = "Nets";
+            // 
+            // mavericksToolStripMenuItem
+            // 
+            this.mavericksToolStripMenuItem.Name = "mavericksToolStripMenuItem";
+            this.mavericksToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.mavericksToolStripMenuItem.Text = "Mavericks";
+            // 
+            // thunderToolStripMenuItem
+            // 
+            this.thunderToolStripMenuItem.Name = "thunderToolStripMenuItem";
+            this.thunderToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.thunderToolStripMenuItem.Text = "Thunder";
+            // 
+            // cavsToolStripMenuItem
+            // 
+            this.cavsToolStripMenuItem.Name = "cavsToolStripMenuItem";
+            this.cavsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cavsToolStripMenuItem.Text = "Cavs";
+            // 
+            // ersToolStripMenuItem
+            // 
+            this.ersToolStripMenuItem.Name = "ersToolStripMenuItem";
+            this.ersToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ersToolStripMenuItem.Text = "76ers";
+            // 
+            // warriorsToolStripMenuItem
+            // 
+            this.warriorsToolStripMenuItem.Name = "warriorsToolStripMenuItem";
+            this.warriorsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.warriorsToolStripMenuItem.Text = "Warriors";
+            // 
+            // bullsToolStripMenuItem
+            // 
+            this.bullsToolStripMenuItem.Name = "bullsToolStripMenuItem";
+            this.bullsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bullsToolStripMenuItem.Text = "Bulls";
+            // 
+            // frmSchoolMascot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1019, 582);
+            this.Controls.Add(this.lblMagic);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmSchoolMascot";
+            this.Text = "NBA Teams";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem schoolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem magicToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem celticsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuggetsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bucksToolStripMenuItem;
+        private System.Windows.Forms.Label lblMagic;
+        private System.Windows.Forms.ToolStripMenuItem raptorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lakersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clipppersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sunsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hawksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem heatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hornetsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rocketsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spursToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem knicksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem netsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mavericksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thunderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cavsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem warriorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bullsToolStripMenuItem;
+    }
+}
+
---
+++ SchoolMascotMagnusJanda/SchoolMascotForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolMascotMagnusJanda
+{
+    public partial class frmSchoolMascot : Form
+    {
+        public frmSchoolMascot()
+        {
+            InitializeComponent();
+        }
+
+        private void schoolsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void magicToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "Orlando Magic";
+        }
+
+        private void celticsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "Boston Celtics";
+        }
+
+        private void nuggetsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "Denver Nuggets";
+        }
+
+        private void bucksToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "Milwakee Bucks";
+        }
+
+        private void raptorsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "Toronto Raptors";
+        }
+
+        private void lakersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "LA Lakers";
+        }
+
+        private void clipppersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "LA Clippers";
+        }
+
+        private void kingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "Sacremento Kings";
+        }
+
+        private void sunsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "Pheonix Suns";
+        }
+
+        private void hawksToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "Atlanta Hawks";
+        }
+
+        private void heatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "Miami Heat";
+        }
+
+        private void hornetsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            lblMagic.Text = "Charlotte Hornets";
+        }
+
+        private void rocketsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "Houston Rockets";
+        }
+
+        private void spursToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lblMagic.Text = "San Antonio Spurs";
+        }
+    }
+}
